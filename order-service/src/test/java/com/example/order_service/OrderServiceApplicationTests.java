9856a4d7729aeff7cec35a750008ec146cb62d11
espeonest <145805393+espeonest@users.noreply.github.com>
2026-02-06 17:11:16 -0500

    docker stuff

--- order-service/src/test/java/com/example/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/order_service/OrderServiceApplicationTests.java
@@ -2,12 +2,21 @@ package com.example.order_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class OrderServiceApplicationTests {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
 	@Test
 	void contextLoads() {
 	}
+
+//    @Test
+//    public void badRequestTest(){
+//        String result = restTemplate.postForObject("http://localhost:8082/orders", null, String.class);
+//        assert (result == "");
+//    }
 
 }
