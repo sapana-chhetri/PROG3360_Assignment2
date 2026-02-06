f8dab4127f67939674a967661c3e72bc57683ec6
BrittRog <151680575+BrittRog@users.noreply.github.com>
2026-02-06 10:41:22 -0500

    Revert "Started Order files"
    This reverts commit fd63b4e0ad8b7faa01f3bb488554ca9d2653cd67.

--- product-service/src/main/java/com/example/product_service/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.product_service.Repository;
-
-import com.example.product_service.Entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
-
-
