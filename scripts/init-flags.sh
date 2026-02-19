fac85c49dd0a789a2fe4430868bbadcb04c05247
BrittRog <151680575+BrittRog@users.noreply.github.com>
2026-02-19 09:47:00 -0500

    Starting Init-Flags Script to enable Feature Flags

---
+++ scripts/init-flags.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#configuration
+ADMIN_TOKEN="" ##Where to get this??
+
+# Create feature flag via Unleash Admin API
+#Feature Flag 1 premium-pricing
+curl -X POST http://localhost:4242/api/admin/projects/default/features  \
+  -H "Authorization: Bearer $ADMIN_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "premium-pricing",
+    "description": "Enable premium pricing discounts 10%",
+    "type": "release"
+  }'
+
+  # Enable premium-pricing flag
+curl -X POST "http://localhost:4242/api/admin/projects/default/features/premium-pricing/environments/development/on" \
+  -H "Authorization: Bearer $ADMIN_TOKEN"
+
+echo "premium-pricing created and turned on"
+
+#Feature Flag 2 order-notifications
+curl -X POST http://localhost:4242/api/admin \
+  -H "Authorization: Bearer $ADMIN_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "name": "order-notifications",
+    "description": "Enable log order confirmation notifications",
+    "type": "release"
+  }'
+
+echo "order-notifications created and turned on"
+  #Feature Flag 3 bulk-order-discount
+  curl -X POST http://localhost:4242/api/admin \
+    -H "Authorization: Bearer $ADMIN_TOKEN" \
+    -H "Content-Type: application/json" \
+    -d '{
+      "name": "bulk-order-discount",
+      "description": "Enable 15% bulk discount when item quantity > 5",
+      "type": "release"
+    }'
+
+echo "bulk-order-discount created and turned on"
