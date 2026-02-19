bd17809f5b283802bf97f95f7e78725480c904bd
BrittRog <151680575+BrittRog@users.noreply.github.com>
2026-02-18 19:43:50 -0500

    Added Logic for FF 3(bulk discount) to order controller, still need to fix bean issue on the featureflagservice

--- order-service/src/main/java/com/example/order_service/Controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/Controller/OrderController.java
@@ -1,18 +1,23 @@
 package com.example.order_service.Controller;
 
 import com.example.order_service.Entity.Order;
+import com.example.order_service.Service.FeatureFlagService;
 import com.example.order_service.Service.OrderService;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
     @RequestMapping("/api")
     public class OrderController {
+
+    private final FeatureFlagService featureFlagService;
     private final OrderService orderService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(FeatureFlagService featureFlagService, OrderService orderService) {
+        this.featureFlagService = featureFlagService;
         this.orderService = orderService;
     }
 
@@ -28,6 +33,19 @@ import java.util.Optional;
 
     @PostMapping("/orders")
     public String addOrder( @RequestBody Order order) {
+
+        //Calculate total order price
+        double totalPrice = order.getTotalPrice() * order.getQuantity();
+
+        //If feature flag 3 is ON and more than 5 items add 15% discount
+        if(featureFlagService.isBulkOrderDiscountEnabled() && order.getQuantity() > 5){
+            totalPrice = totalPrice - (totalPrice * 0.15);
+        }
+
+        //If bulk-order-discount flag is OFF: Calculate totalPrice regardless of quantity
+        order.setTotalPrice(totalPrice);
+
+        //Save / Place order
         if(orderService.addOrder(order)) {
             return "Order placed successfully";
         }
--- order-service/src/main/java/com/example/order_service/Service/FeatureFlagService.java
+++ order-service/src/main/java/com/example/order_service/Service/FeatureFlagService.java
@@ -1,16 +1,29 @@
 package com.example.order_service.Service;
 
 import io.getunleash.Unleash;
+import org.springframework.stereotype.Service;
 
+@Service
 public class FeatureFlagService {
 
     private final Unleash unleash;
 
+    //Constructor injection for the Unleash client
     public FeatureFlagService(Unleash unleash) {
         this.unleash = unleash;
     }
 
     //Feature Flag 2 Order Notifications
 
+
     //Feature Flag 3 Bulk Order Discount
+    public boolean isBulkOrderDiscountEnabled() {
+       try {
+           return unleash.isEnabled("bulk-order-discount", false);
+       } catch (Exception e) {
+           //proper error handling for Unleash connection failures
+           return false;
+       }
+    }
+
 }
