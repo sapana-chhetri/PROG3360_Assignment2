fa485cfabca38278498c89e369f9990bb5967a80
BrittRog <151680575+BrittRog@users.noreply.github.com>
2026-02-18 20:36:33 -0500

    Moving FF3 logic to order service rather than controller

--- order-service/src/main/java/com/example/order_service/Controller/OrderController.java
+++ order-service/src/main/java/com/example/order_service/Controller/OrderController.java
@@ -13,11 +13,9 @@ import java.util.Optional;
     @RequestMapping("/api")
     public class OrderController {
 
-    private final FeatureFlagService featureFlagService;
     private final OrderService orderService;
 
     public OrderController(FeatureFlagService featureFlagService, OrderService orderService) {
-        this.featureFlagService = featureFlagService;
         this.orderService = orderService;
     }
 
@@ -33,17 +31,6 @@ import java.util.Optional;
 
     @PostMapping("/orders")
     public String addOrder( @RequestBody Order order) {
-
-        //Calculate total order price
-        double totalPrice = order.getTotalPrice() * order.getQuantity();
-
-        //If feature flag 3 is ON and more than 5 items add 15% discount
-        if(featureFlagService.isBulkOrderDiscountEnabled() && order.getQuantity() > 5){
-            totalPrice = totalPrice - (totalPrice * 0.15);
-        }
-
-        //If bulk-order-discount flag is OFF: Calculate totalPrice regardless of quantity
-        order.setTotalPrice(totalPrice);
 
         //Save / Place order
         if(orderService.addOrder(order)) {
