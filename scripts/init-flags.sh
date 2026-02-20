3fa792da2046159752bf9475d38b75f77fce9028
sapana-chhetri <122324804+sapana-chhetri@users.noreply.github.com>
2026-02-20 12:52:44 -0500

    Update Unleash API endpoint to localhost

--- scripts/init-flags.sh
+++ scripts/init-flags.sh
@@ -7,7 +7,7 @@ ADMIN_TOKEN="user:e95094979e08f374ebb77fa1b546ed3ce46a5c5ce985bfd626519526"
 # Create feature flag via Unleash
 # Admin API
 #Feature Flag 1 premium-pricing
-curl -X POST http://unleash_server:4242/api/admin/projects/default/features  \
+curl -X POST http://localhost:4242/api/admin/projects/default/features  \
   -H "Authorization: $ADMIN_TOKEN" \
   -H "Content-Type: application/json" \
   -d '{
@@ -17,7 +17,7 @@ curl -X POST http://unleash_server:4242/api/admin/projects/default/features  \
   }'
 
   # Enable premium-pricing flag
-curl -X POST "http://unleash_server:4242/api/admin/projects/default/features/premium-pricing/environments/development/on" \
+curl -X POST "http://localhost:4242/api/admin/projects/default/features/premium-pricing/environments/development/on" \
   -H "Authorization: $ADMIN_TOKEN"
 
 echo "premium-pricing created and turned on"
