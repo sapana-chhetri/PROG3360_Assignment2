5dc05c60451452468076d19958dec18682c98f76
deathn-t <145395624+deathn-t@users.noreply.github.com>
2026-02-20 10:10:01 -0500

    rest template fix 1/3

--- order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order_service/OrderServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.unleash.features.config.UnleashAutoConfiguration;
 
 @SpringBootApplication(exclude = {UnleashAutoConfiguration.class})
